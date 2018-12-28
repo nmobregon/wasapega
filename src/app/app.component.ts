85fb5e1d39328bad9af0eda3a73e143982762c83
Nicolas Mariano Obregon <nmobregon@thinkup.com.ar>
2018-12-28 12:07:47 -0300

    fixes varios

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,58 @@
 import { Component } from '@angular/core';
+import { HistoryNumber } from './history-number.class';
+import { TranslateService } from '@ngx-translate/core';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+	selector: 'app-root',
+	templateUrl: './app.component.html',
+	styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'ng-wasapega';
+	title = 'Wasape.ga';
+	code:string = "54";
+	num:string;
+	history:HistoryNumber[] = undefined;
+	loading:boolean = true;
+
+	constructor(public translate: TranslateService) {
+
+        translate.setDefaultLang('es');
+
+		let numbers = localStorage.getItem("wasapega");
+		this.history = numbers ? JSON.parse(numbers).map(x=>new HistoryNumber(x.number, new Date(x.date))) : [];
+
+		translate.use('es');
+		setTimeout(x=>this.loading=false,500);
+
+	}
+
+	public send = ()=>{
+		let number = this.code.replace("+","")+""+this.num;
+		this.save(number);
+		this.writeTo(number);
+		this.code = "54";
+		this.num = undefined;
+	};
+	
+	public save = (num) => {
+		this.history.push(new HistoryNumber(num, new Date()));
+		localStorage.setItem("wasapega", JSON.stringify(this.history));
+	};
+	
+	public writeTo = (num)=>{
+		window.open("https://wa.me/"+num, "_blank","noreferrer");
+	};
+	
+	public clean = ()=>{
+		localStorage.removeItem("wasapega");
+		this.history = [];
+	}
+
+	set locale(locale:string){
+		this.translate.use(locale);
+	}
+	
+	get historyNumbers(){
+		return history && this.history.length ? this.history : undefined;
+	}
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,39 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { MomentModule } from 'ngx-moment';
+import { HttpClientModule, HttpClient } from '@angular/common/http';
+import { TranslateModule, TranslateLoader } from '@ngx-translate/core';
+import { TranslateHttpLoader } from '@ngx-translate/http-loader';
 
 import { AppComponent } from './app.component';
+import { ServiceWorkerModule } from '@angular/service-worker';
+import { environment } from '../environments/environment';
+
+// AoT requires an exported function for factories
+export function HttpLoaderFactory(http: HttpClient) {
+	return new TranslateHttpLoader(http, './assets/i18n/', '.json');
+}
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+	declarations: [
+		AppComponent
+	],
+	imports: [
+		BrowserModule,
+		FormsModule,
+		MomentModule,
+		HttpClientModule,
+		TranslateModule.forRoot({
+			loader: {
+				provide: TranslateLoader,
+				useFactory: HttpLoaderFactory,
+				deps: [HttpClient]
+			}
+		}),
+		ServiceWorkerModule.register('ngsw-worker.js', { enabled: environment.production })
+	],
+	providers: [],
+	bootstrap: [AppComponent]
 })
 export class AppModule { }
