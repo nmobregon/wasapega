247b10bcb8240faef5dadc688738db8ced73cdbd
Nicolas Mariano Obregon <nmobregon@thinkup.com.ar>
2018-12-28 17:47:39 -0300

    Fix in history tooltip

--- src/app/model/history-number.class.ts
+++ src/app/model/history-number.class.ts
@@ -2,4 +2,7 @@ export class HistoryNumber {
 
 	constructor(public number:string, public date:Date, public name?:string){}
 	
+	get tooltip(){
+		return this.name ? (this.name + " (" + this.number + ")") : this.number;
+	}
 }
